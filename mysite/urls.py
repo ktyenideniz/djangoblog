83a35d2d55531ea7f613cbec2a48d53d5cd8e5c5
ktyenideniz <yenkaa.21@robcol.k12.tr>
2018-08-06 20:14:22 +0300

    ikinci commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,14 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+import sys
+sys.path.append(sys.path[0] + "/../..")
+
+from django.urls import path
 from . import settings
+from blog import views
 from django.contrib.staticfiles.urls import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
+
 
 urlpatterns += staticfiles_urlpatterns()
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
